427113bf57993d1aa06a24ebb33cace8ad7ef4bc
LewinUp <raft.took@yandex.ru>
2023-11-13 20:53:58 +0300

    feat: change fontSize && fix: footer

--- packages/frontend/src/app/UIkit/PageLayout/PageLayout.tsx
+++ packages/frontend/src/app/UIkit/PageLayout/PageLayout.tsx
@@ -5,7 +5,7 @@ export const PageLayout = styled(Container, {
   paddingTop: 'calc($layout$navBarHeight + $space$4)',
   paddingBottom: 48,
   backgroundColor: '$gray100',
-  minHeight: '100%',
+  minHeight: '100vh',
   position: 'relative',
   variants: {
     nonePaddingTop: {
